f3d26b222960d23fc4d410ffae057af470ae6afb
TeoResende <resendenetto@yahoo.com.br>
2016-10-11 16:30:17 -0300

    Novo método Qual o é o meu nome

--- GitHub/src/br/com/senai/Aula1.java
+++ GitHub/src/br/com/senai/Aula1.java
@@ -5,15 +5,18 @@ import javax.swing.JOptionPane;
 public class Aula1 {
 	
 	static String msg = "Bem vindo ao GitHub";
+	static String meuNome = "Téo";
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		JOptionPane.showMessageDialog(null,msg);
-
 	}
 	
 	static void trocaMsg(){
 		msg = JOptionPane.showInputDialog("Entre com uma nova mensagem ");
+	}
+	
+	static void qualSeuNome(){
+		JOptionPane.showMessageDialog(null,meuNome);
 	}
 
 }
